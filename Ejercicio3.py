7ee3e74ae1ea5e24dd10374692b0a1d8a1f34fde
DiegoAGomezS <dagomez@uamv.edu.ni>
2025-05-13 10:01:54 -0600

    Ejercicio 3 marcado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,24 +1,30 @@
+#Numero de estudiantes a evaluar
 estudiantes_evaluados = int(input("Ingrese los estudiantes a calificar: "))
 
+#Seleccionador de estudiantes
 for estudiante in range(1, estudiantes_evaluados + 1):
         print(f"\n--- Estudiante #{estudiante} ---")
         promedio_general = 0
 
+#Selector de asignatura
 for asignatura in range(1,4):
         print(f"\n Asignatura: {asignatura}")
         suma_notas = 0
         
+        #Notas de tareas de la asignatura
         for tarea in range (1, 4):
                 sum_nota = 0
                 notas = float(input(f"Ingrese la nota de la tarea {tarea}: "))
                 sum_nota += notas
-                
+        #Nota del examen de la asignatura
         nota_examen = float(input("Ingrese la nota de su examen: "))
         suma_notas += nota_examen
         
+        #Promedio total de la asignatura
         promedio_asignatura = suma_notas / 4
         print(f"Promedio de asignatura: {promedio_asignatura:.2f}")
         promedio_general += promedio_asignatura
-        
+
+#Promedio total de todas las asignaturas
 promedio_general /= 3
 print(f"\nEl promedio general del estudiante es: {promedio_general:.2f}")
