db91c18bb083d1a6df912ce4a22d476c70bd35ca
LuisCReyes <lcreyes@uamv.edu.ni>
2025-05-13 08:26:37 -0600

    Caso8

---
+++ caso8.py
@@ -0,0 +1,5 @@
+""" Caso 8: Registro de consumo eléctrico por edificio en la UAM
+Desarrolle un programa que permita registrar el consumo eléctrico de cinco edificios del campus
+UAM (como aulas, biblioteca, administración, laboratorios y cafetería) durante tres turnos del
+día (mañana, tarde y noche), por una semana. El programa debe mostrar el consumo por edificio
+y el total general semanal """
