f4f1f5c9d4484a0e5d94e8fef21dcb11a9cc3d24
LuisCReyes <lcreyes@uamv.edu.ni>
2025-05-13 08:26:21 -0600

    Caso7

---
+++ caso7.py
@@ -0,0 +1,6 @@
+""" Caso 7: Control de ventas en kioscos estudiantiles de la UAM
+Implemente un programa que simule el control de ventas de alimentos en tres kioscos dentro
+del campus UAM. Cada kiosco ofrece cinco productos diferentes y se registrarán las ventas
+durante cuatro días. El programa debe calcular y mostrar el total vendido por producto en cada
+kiosco, así como el total general por día. """
+
